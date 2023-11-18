e74de0d57b091fa22a4fdd4124a1636806794bb4
Akriti Sengar <akritisengarr@gmail.com>
2023-11-18 15:31:04 +0530

    Added dark mode

--- scricpt.js
+++ scricpt.js
@@ -35,3 +35,19 @@ const navLinks = document.getElementById("nav-links");
 toggleButton.addEventListener("click", () => {
                 navLinks.classList.toggle("active");
             });
+
+//gets the button by ID from your HTML element
+const themeBtn = document.getElementById("theme-btn");
+//when you click that button
+themeBtn.onclick = () => {
+//the default class "fa-moon" switches to "fa-sun" on toggle
+  themeBtn.classList.toggle("fa-sun");
+//after the switch on toggle, if your button contains "fa-sun" class
+  if (themeBtn.classList.contains("fa-sun")) {
+//onclicking themeBtn, the changeTheme styling will be applied to the body of your HTML
+    document.body.classList.add("changeTheme");
+  } else {
+// onclicking themeBtn, applied changeTheme styling will be removed
+    document.body.classList.remove("changeTheme");
+  }
+}
